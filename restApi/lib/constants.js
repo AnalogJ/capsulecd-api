63741e2f8fb64d10fbc9e6553e334a9472310b51
Jason Kulatunga <jk17@ualberta.ca>
2016-07-13 18:40:44 -0700

    fixed the endpoints that we're looking for.

--- restApi/lib/constants.js
+++ restApi/lib/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     //this is the api endpoint for application once its been uploaded to lambda
-    'lambda_endpoint': 'https://api.capsulecd.com/dev',
+    'lambda_endpoint': 'https://api.capsulecd.com/prod',
     'web_endpoint': 'http://www.capsulecd.com',
     'capsulecd_username': 'CapsuleCD'
 }
