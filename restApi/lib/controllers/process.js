4e5075e353be2235ef47aa69240c7fc0a85698e6
Jason Kulatunga <jk17@ualberta.ca>
2016-07-14 19:53:46 -0700

    added multiple engines, so we can test hyper and docker cloud at the same time.

--- restApi/lib/controllers/process.js
+++ restApi/lib/controllers/process.js
@@ -1,6 +1,5 @@
 var security = require('../security');
 var Helpers = require('../helpers');
-var request = require('superagent');
 var q = require('q');
 
 var AWS = require("aws-sdk");
@@ -12,50 +11,7 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 var table = process.env.SERVERLESS_DATA_MODEL_STAGE + '-' + process.env.SERVERLESS_PROJECT + '-projects';
 
 
-var configuration = {
-    baseEndpoint: 'https://cloud.docker.com/api/app/v1',
-    username: 'analogj',
-    apiKey: process.env.TUTUM_API_KEY
-};
 
-configuration.authorization = new Buffer(configuration.username+':'+configuration.apiKey).toString('base64');
-
-
-
-function tutum_request(method, path, params, callback) {
-    var body = {};
-
-    var url = configuration.baseEndpoint;
-
-    // Add leading and trailing slashes to the path if they do not exist
-    url += path.replace(/^\/?/, '/').replace(/\/?$/, '/');
-
-    callback = (typeof params === 'function') ? params : callback;
-
-    method = method.toLowerCase();
-    method = (method === 'delete') ? 'del' : method;
-
-    if (method === 'post' || method === 'patch') {
-        body = params;
-    } else {
-       // url += '?' + querystring.stringify(params);
-    }
-
-    request[method](url).
-    send(body).
-    set('Accept', 'application/json').
-    set('Authorization', 'Basic ' + configuration.authorization).
-    set('Content-Type', 'application/json').
-    end(function(err, res){
-            if (err) {
-                return callback(err);
-            }
-            if (res.error) {
-            return callback(res.error);
-        }
-        callback(null, res.body);
-    });
-};
 
 function findProject(auth, serviceType, orgId, repoId){
     var params = {
@@ -89,54 +45,7 @@ module.exports = function (event, cb) {
         .then(function(decoded){
             return findProject(decoded, event.serviceType, event.orgId, event.repoId)
         })
-        .then(function(project_data){
-            var project = project_data.project;
-            var token = project_data.token;
-
-            var date_prefix = new Date().toISOString()
-                .replace(/T/, '-')      // replace T with a space
-                .replace(/\..+/, '')     // delete the dot and everything after
-                .replace(/:/g,'-');
-
-
-            var env_vars = [];
-            //loop through secrets, decrypt and set on the env.
-            var keys = Object.keys(project.Secrets);
-            for(var ndx in keys){
-                var key = keys[ndx];
-                if(key == 'CAPSULE_RUNNER_PULL_REQUEST' || key == 'CAPSULE_RUNNER_REPO_FULL_NAME'){ continue; }
-                var decrypted_value = security.decrypt(project.Secrets[key].enc_value)
-                env_vars.push({"key":key, "value":decrypted_value});
-            }
-            //set values here
-            env_vars.push({"key":"CAPSULE_RUNNER_PULL_REQUEST","value":event.prNumber});
-            env_vars.push({"key":"CAPSULE_RUNNER_REPO_FULL_NAME","value":project.OrgId + '/' + project.RepoId});
-            //TODO: this key should be dynamically created for each project, and removed automaticaly afterwards.
-            env_vars.push({"key":"CAPSULE_SOURCE_GITHUB_ACCESS_TOKEN","value":token});
-
-            var data = {
-                "nickname": date_prefix + '-' + event.serviceType + '-' + event.orgId + '-' + event.repoId + '-' + event.prNumber,
-                "image": project.Settings.dockerImage,
-                "run_command": "capsulecd start --source "+event.serviceType+" --package_type " + project.Settings.packageType,
-                "container_envvars": env_vars,
-                "target_num_containers": 1,
-                "autodestroy":"ALWAYS"
-            };
-            var service_deferred = q.defer();
-            tutum_request('POST','/service/', data, function(err, res) {
-                if (err)  return service_deferred.reject(err);
-                return service_deferred.resolve(res);
-            });
-            return service_deferred.promise
-        })
-        .then(function(service){
-            var start_deferred = q.defer();
-            tutum_request('POST', '/service/' + service.uuid + '/start', {}, function(err, res) {
-                if (err)  return start_deferred.reject(err);
-                return start_deferred.resolve(res);
-            });
-            return start_deferred.promise
-        })
+        .then(require('../engines/dockercloud'))
         .then(function(payload){
             //return it to the callback
             //TODO: we're sending back an empty object so we dont accidently leak tutum keys.
