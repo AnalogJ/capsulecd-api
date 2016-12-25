2e17605395ecf290ba04972c92852bfe33928410
Jason Kulatunga <jk17@ualberta.ca>
2016-12-25 11:20:41 -0700

    trying to mix and match python.

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,7 @@
 module.exports = {
     //this is the api endpoint for application once its been uploaded to lambda
-    'lambda_endpoint': 'https://api.capsulecd.com/'+ process.env.API_VERSION,
+    //if stage is pointing to the master branch, this is V1, else, its the beta version of the api.
+    'lambda_endpoint': 'https://api.capsulecd.com/'+ (process.env.STAGE=='master' ? 'v1' : process.env.STAGE),
     'web_endpoint': 'http://www.capsulecd.com',
     'capsulecd_username': 'CapsuleCD'
 }
