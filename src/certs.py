b978a76a52ad4d59e75da77e2f8cd682378ff4a3
Jason Kulatunga <jk17@ualberta.ca>
2016-12-25 11:23:57 -0700

    check if hte awscli is on the path.

--- src/certs.py
+++ src/certs.py
@@ -1,8 +1,10 @@
 import json
 import datetime
-
+import distutils.spawn
 
 def renew(event, context):
+    if not distutils.spawn.find_executable('aws'): raise StandardError('aws cli is not available on the path')
+
     current_time = datetime.datetime.now().time()
     body = {
         "message": "Hello, the current time is " + str(current_time)
