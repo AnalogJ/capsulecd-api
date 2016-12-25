5035946b62d18f29e996f88a9af7e256d435093e
Jason Kulatunga <jk17@ualberta.ca>
2016-12-25 11:47:43 -0700

    added cleanup (with enable/disable flaG)

---
+++ src/cleanup.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports.index = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Go Serverless v1.0! Your function executed successfully!',
+            input: event,
+            'apiSha1': process.env.API_SHA1
+
+        }),
+    };
+
+    callback(null, response);
+
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
