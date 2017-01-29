6be6694caeac173a68cafeaa5b86ed8b24c52432
Jason Kulatunga <jk17@ualberta.ca>
2017-01-28 18:54:23 -0800

    fix version info.

--- src/version.js
+++ src/version.js
@@ -10,7 +10,7 @@ module.exports.handler = (event, context, callback) => {
     console.log(versionInfo)
 
 
-    callback(null, versionInfo);
+    return callback(null, versionInfo);
 
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
