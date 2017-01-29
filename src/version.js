2f129bd111d222c7f8478c4a87151a99144b5363
Jason Kulatunga <jk17@ualberta.ca>
2017-01-28 18:22:52 -0800

    correct deploy sha.

--- src/version.js
+++ src/version.js
@@ -1,17 +1,9 @@
 'use strict';
 
 module.exports.handler = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: event,
-            'apiSha1': process.env.API_SHA1
-
-        }),
-    };
-
-    callback(null, response);
+    callback(null, {
+        'deploySha': process.env.DEPLOY_SHA
+    });
 
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
